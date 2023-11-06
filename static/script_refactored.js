1531037b9be51e0b44389912f60d703f3e80a652
AleksisLesieur <Aleksisjonaslesieur@gmail.com>
2023-11-07 01:40:13 +0200

    updating

--- static/script_refactored.js
+++ static/script_refactored.js
@@ -1,5 +1,7 @@
 const board = {}
 
+let receivedData;
+
 const currentData = {}
 
 let currentPlayer = 'X' // true stands for "X" symbol
@@ -17,29 +19,10 @@ async function sendData() {
       },
       body: JSON.stringify(currentData),
     });
-    const result = await response.json()
-    console.log(result)
+    receivedData = await response.json();
 }
 
-
-// async function updateData() {
-//   const response = await fetch("/", {
-//     method: "PUT",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(currentData),
-//   });
-//   const result = await response.json();
-//   console.log(result, " testing");
-// }
-
-// async function getData() {
-//   const response = await fetch("http://127.0.0.1:8000/");
-//   const data = await response.json()
-//   console.log(data)
-//   console.log('getdata function')
-// }
+console.log(receivedData)
 
 const selectBoard = document.querySelectorAll('.box')
 
@@ -47,20 +30,21 @@ for (let i = 1; i < selectBoard.length + 1; i++) {
   selectBoard[i - 1].addEventListener('click', function () {
     if (currentPlayer === "X" && currentData[`box${i}`] === null) {
       currentData[`box${i}`] = "X";
+      selectBoard[i - 1].textContent = receivedData[`box${i}`]
       currentPlayer = "O";
-      selectBoard[i - 1].textContent = "X"
     }
     else if (currentPlayer === "O" && currentData[`box${i}`] === null) {
       currentData[`box${i}`] = "O";
       currentPlayer = "X";
-      selectBoard[i - 1].textContent = "O";
+      selectBoard[i - 1].textContent = receivedData[`box${i}`];
     }
     sendData(currentData);
-    })
+  })
 }
 
 // Using setInterval in JavaScript
-// setInterval(() => {
-//   sendData(currentData);
-//   console.log("chat");
-// }, 1000); // 1000 milliseconds = 1 second
+setInterval(() => {
+  sendData(currentData);
+  console.log("test");
+}, 1000); 
+// 1000 milliseconds = 1 second
