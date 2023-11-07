e629399bfc1ab17becb923d962bd83706c977bfd
AleksisLesieur <Aleksisjonaslesieur@gmail.com>
2023-11-07 04:06:53 +0200

    updated

--- main.py
+++ main.py
@@ -30,7 +30,8 @@ app.add_middleware(
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory="./templates")
+templates = Jinja2Templates(directory="templates")
+
 
 class BoxData(BaseModel):
     box1: Optional[str] = None
